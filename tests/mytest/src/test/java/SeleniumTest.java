e768a8cc5a29d2199f6c3981a1dc217fa28d90da
pecku <xsajtx@gmail.com>
2023-05-26 07:56:06 +0200

    Initialize project

---
+++ tests/mytest/src/test/java/SeleniumTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import java.util.*;  
+
+import java.net.URL;
+import java.net.MalformedURLException;
+
+
+public class SeleniumTest {
+    public WebDriver driver;
+    
+    @Before
+    public void setup()  throws MalformedURLException  {
+        ChromeOptions options = new ChromeOptions();
+        driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+        driver.manage().window().maximize();
+    }
+    
+
+    
+    @After
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
